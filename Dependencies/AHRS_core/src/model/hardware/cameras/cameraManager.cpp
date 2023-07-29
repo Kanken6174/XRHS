f9f063f1dfff9983e0448a334a39d31f7dd67b74
yorick.geoffre <yorick.geoffre@etu.uca.fr>
2023-07-29 12:24:55 +0200

    trying camera logic fixes

--- Dependencies/AHRS_core/src/model/hardware/cameras/cameraManager.cpp
+++ Dependencies/AHRS_core/src/model/hardware/cameras/cameraManager.cpp
@@ -25,45 +25,54 @@ void cameraManager::runCaptureForCamera(camera* c, uint index){
     }
 }
 
-cameraManager::cameraManager(){
-    std::cout << "init camera manager" << endl;
-    vector<std::string> cameras = listCameras();
-    uint cameraIndex = 0;
-    std::cout << "found " << cameras.size() << " cameras" << endl;
+cameraManager::cameraManager() {
+    std::cout << "init camera manager" << std::endl;
+    std::vector<std::string> cameras = listCameras();
+    std::unordered_set<int> invalid_cameras;
 
-    for(string camIdx : cameras)    //camera input testing
-    {
-        int camID = stoi(camIdx);
-        try{
-            VideoCapture vs(camID);
-            if(!vs.isOpened() && !vs.open(camID))
-            {
-                cout << "\x1B[31opening camera " << camIdx << " failed\033[0m" <<endl;
-                vs.release();
-            }else{
-            vs.release();
-            camera* cam = new camera();
-            cam->source = new VideoCapture(camID, CAP_FFMPEG);
-            cam->source->set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
-            cam->source->set(CV_CAP_PROP_FPS , FPS_LIMIT_PSEYE);
-            cam->source->set(CAP_PROP_FRAME_WIDTH, 1920);
-            cam->source->set(CAP_PROP_FRAME_HEIGHT, 1080);
-            
-
-            cam->path = camID;
-            videoSources.push_back(cam);    //valid camera added
-            cout << "added camera with path /dev/video" << cam->path << endl;
-            UMat m;
-            captures.push_back(std::move(m));             
-            }
-        }
-        catch(exception e){
-            cout << e.what() << endl;
+    // Parse invalid camera indices
+    for(const auto& camIdx : cameras) {
+        if(camIdx.find("dec") != std::string::npos || camIdx.find("enc") != std::string::npos) {
+            std::string index_str = camIdx.substr(camIdx.find_last_of("-") + 1);
+            invalid_cameras.insert(std::stoi(index_str));
         }
     }
 
-    cout << "found " << std::to_string(videoSources.size()) << " valid cameras" << endl;
+    std::cout << "found " << cameras.size() << " cameras" << std::endl;
+
+    for(std::string camIdx : cameras) {
+        int camID = std::stoi(camIdx);
+        // If the camera index is invalid, skip it
+        if(invalid_cameras.count(camID)) continue;
+
+        try {
+            cv::VideoCapture vs(camID);
+            if(!vs.isOpened() || !vs.open(camID)) {
+                std::cout << "\x1B[31mopening camera " << camIdx << " failed\033[0m" << std::endl;
+                vs.release();
+            } else {
+                vs.release();
+                camera* cam = new camera();
+                cam->source = new cv::VideoCapture(camID, cv::CAP_FFMPEG);
+                cam->source->set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
+                cam->source->set(CV_CAP_PROP_FPS , FPS_LIMIT_PSEYE);
+                cam->source->set(cv::CAP_PROP_FRAME_WIDTH, 1920);
+                cam->source->set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
+
+                cam->path = camID;
+                videoSources.push_back(cam);    //valid camera added
+                std::cout << "added camera with path /dev/video" << cam->path << std::endl;
+                cv::UMat m;
+                captures.push_back(std::move(m));
+            }
+        } catch(std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    std::cout << "found " << std::to_string(videoSources.size()) << " valid cameras" << std::endl;
 }
+
 
 void cameraManager::runCapture(){
     cout << "cameraManager::runCapture------------------------------------------------\n" << "enabling capture thread for " << videoSources.size() << " cameras" << endl;
